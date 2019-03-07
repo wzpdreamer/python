b136fe9b97112d565c3eff0da02647e3753d52a7
wzpdreamer <3176869767@qq.com>
2019-03-07 20:48:30 +0800

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+print("hello world!")
+print("我爱你")
+print("SC",5.0*6.0)
+print(5<6)
+num=61
+print("You are my %s!"%num)
+print("Forever!")
+print("#",123456789)
+my_name='Zed A.Show'
+print(f"My name is {my_name}.") 
+print(round(3.1415926))
+w="xfjljjlhf"
+z="gnfxnhnnn"
+p="nfdfbbnff"
+print(w+z+p)
+print(f"I said :'{w}'")
+print("Its fleece was while as {}.".format('snow'))
+print("." * 10)
+ed1='C'
+ed2='h'
+ed3='e'
+ed4='e'
+ed5='s'
+ed6="e"
+ed7='B'
+ed8='u'
+ed9='r'
+ed10='g'
+ed11='e'
+ed12='r'
+print(ed1+ed2+ed3+ed4+ed5+ed6,end=' ')
+print(ed7+ed8+ed9+ed10+ed11+ed12)
+
+
